250b23af6cd78e8ed82f9b1e9680524861dbe8a7
Ansel-dal <82544423+Ansel-dal@users.noreply.github.com>
2022-11-24 23:04:40 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -64,7 +64,7 @@ sudo chmod ugo+rwx display.desktop
 #modifico archivo y agrego instrucciones
 echo "[Desktop Entry]
 Name=KioskMode #name
-Exec=chromium --start-fullscreen --force-device-scale-factor=0.7 --disable-pinch  --kiosk --app=http://localhost:5022/
+Exec=chromium-browser --start-fullscreen --force-device-scale-factor=0.7 --kiosk --app=http://localhost:5022/
 " > /etc/xdg/autostart/display.desktop
 
 
